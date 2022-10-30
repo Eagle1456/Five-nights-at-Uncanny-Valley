19a16f05f47ee2e1d1e9d1259bce42d4045bcd4c
Jose L <25176538+luchsj@users.noreply.github.com>
2022-10-30 15:39:15 -0400

    destroy enemies
    kill.

--- Five Nights at Uncanny Valley/Assets/EnemyBehavior.cs
+++ Five Nights at Uncanny Valley/Assets/EnemyBehavior.cs
@@ -8,18 +8,66 @@ public class EnemyBehavior : MonoBehaviour
     GameObject player;
     public float fov = .7f;
     public float speed = 1.0f;
+    float currentSpeed = 0;
+    [SerializeField] float accelerationTime = 1;
+    Rigidbody rb;
+
     void Start()
     {
         player = GameObject.Find("Player"); 
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        //move if player is looking away
-        if (Vector3.Dot(player.transform.forward, transform.position) < 1 - fov)
+        if(currentSpeed != 0)
         {
-            transform.LookAt(player.transform);
-            transform.Translate(-transform.forward * speed * Time.deltaTime); 
+            rb.transform.Translate(-transform.forward * currentSpeed * Time.deltaTime);
+            //Debug.Log(currentSpeed);
         }
+
+        //move if player is looking away
+        if (!(Vector3.Dot(player.transform.forward, transform.position - player.transform.position) > 1 - fov) && currentSpeed <= 0)
+        {
+            rb.transform.LookAt(player.transform);
+            StopCoroutine("MovementDeccelerate");
+            StartCoroutine("MovementAccelerate");
+        }
+        else if(Vector3.Dot(player.transform.forward, transform.position) > 1 - fov && currentSpeed >= speed)
+        {
+            StopCoroutine("MovementAccelerate");
+            StartCoroutine("MovementDeccelerate");
+        }
+    }
+
+    IEnumerator MovementAccelerate()
+    {
+        float t = 0f;
+        while (t < 1.0f)
+        {
+            currentSpeed = Mathf.Lerp(0, speed, t);
+            t += Time.deltaTime / accelerationTime;
+            yield return null;
+        }
+        currentSpeed = speed;
+        yield return null;
+    }
+
+    IEnumerator MovementDeccelerate()
+    {
+        float t = 0f;
+        while (t < 1.0f)
+        {
+            currentSpeed = speed - Mathf.Lerp(0, speed, t);
+            t += Time.deltaTime / accelerationTime;
+            yield return null;
+        }
+        currentSpeed = 0;
+        yield return null;
+    }
+
+    public void OnPlayerHit()
+    {
+        Destroy(gameObject);
     }
 }
--- Five Nights at Uncanny Valley/Assets/Scripts/PlayerController.cs
+++ Five Nights at Uncanny Valley/Assets/Scripts/PlayerController.cs
@@ -8,19 +8,77 @@ using UnityEngine.UIElements;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float playerSpeed = 1.0f;
-
+    public float mouseSensitivity = 2.0f;
+    [SerializeField] float meleeRange = 1.0f;
+    bool hasMelee;
+    GameObject mainCam;
+    Rigidbody rb;
+    
     void Awake()
     {
+        mainCam = GameObject.Find("Main Camera");
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         Vector3 vec_in = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        vec_in = transform.TransformDirection(vec_in);
-        transform.position += vec_in * playerSpeed * Time.deltaTime;
+        vec_in = rb.transform.TransformDirection(vec_in);
+        rb.transform.position += vec_in * playerSpeed * Time.deltaTime;
+         
+        rb.transform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * mouseSensitivity);
+        mainCam.transform.Rotate(Vector3.right, -Input.GetAxis("Mouse Y") * mouseSensitivity);
 
+        //melee attack. raycast for enemy, if hit, destroy weapon and enemy.
+        Debug.DrawRay(mainCam.transform.position, mainCam.transform.forward * 5, Color.red);
+        if (hasMelee && Input.GetKeyDown(KeyCode.Mouse0)) 
+        {
+            Debug.Log("destroy??");
+            RaycastHit hit;
+            //Debug.DrawRay(mainCam.transform.position, mainCam.transform.forward);
+            if(Physics.Raycast(mainCam.transform.position, mainCam.transform.forward, out hit, meleeRange, LayerMask.GetMask("Enemies")))
+            {
+                Debug.Log("kill?");
+                hit.collider.gameObject.GetComponent<EnemyBehavior>().OnPlayerHit();
+                MeleeDestroy();
+            }
+        }
+    }
 
-        transform.Rotate(Vector3.up, Input.GetAxis("Mouse X"));
-        GameObject.Find("Main Camera").transform.Rotate(Vector3.right, -Input.GetAxis("Mouse Y"));
+    void MeleeEquip()
+    {
+        hasMelee = true;
+    }
+
+    void MeleeDestroy()
+    {
+        hasMelee = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            case "Enemy":
+                //guess i'll die
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch(other.gameObject.tag)
+        {
+            case "Melee":
+                MeleeEquip();
+                Destroy(other.gameObject);
+                break;
+
+            default:
+                break;
+        }
     }
 }
